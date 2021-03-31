91375a9538cb15f3c3f25647fc7e37b04c884336
190103203 <190103203@stu.sdu.edu.kz>
2021-03-31 16:42:45 +0600

    lab7 api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\post;
+
+class ApiController extends Controller
+{
+    //
+    public function index(Request $request){
+        $posts = post::all();
+
+        return response($posts, 200);
+    }
+
+    public function get_post(Request $request){
+        $post = post::find($request->post_id);
+
+        if($client == null){
+            return response(['message' => 'There is no post'], 404)
+        }
+
+        return response($post, 200);
+    }
+}
